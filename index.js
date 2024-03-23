eb2e58b0583822ae08f1112ea764f2b1ae15fb1c
mwaikin <mwaikingori10@gmail.com>
2024-03-23 22:06:48 +0300

    complete arithmetic lab

--- index.js
+++ index.js
@@ -1,2 +1,10 @@
 
 // Write your code here
+let num1 = 2;
+let num2 = 31;
+let multiply = num1 * num2;
+let random = Math.ceil(Math.random() *10)+1;
+let num3 = 16;
+let num4 =4;
+let mod = num3/num4;
+let max = Math.max(4,6,9,20);
